592475c856f5e8fe0f71173de5741b6377353a0b
AnnaMarcos <58533836+AnnaMarcos@users.noreply.github.com>
2020-09-23 11:53:59 -0700

    8.4. Lazy helpers initialization

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -14,10 +14,10 @@ import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager  {
   private final Properties properties;
-  WebDriver wd;
+  private WebDriver wd;
 
   private  String browser;
-
+  private RegistrationHelper registrationHelper;
   public ApplicationManager(String browser) {
     this.browser = browser;
     properties = new Properties();
@@ -27,8 +27,33 @@ public class ApplicationManager  {
     public void init() throws IOException {
       String target = System.getProperty("target", "local");
       properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+    }
 
+    public void logout() {
+      wd.findElement(By.linkText("Logout")).click();
+    }
+    public void stop() {
+      if (wd != null) {
+        wd.quit();
+      }
+    }
+  public HttpSession newSession(){
+    return new HttpSession(this);
+  }
 
+  public String getProperty(String key){
+    return properties.getProperty(key);
+  }
+
+  public RegistrationHelper registration() {
+    if (registrationHelper == null) {
+      registrationHelper = new RegistrationHelper(this);
+    }
+    return registrationHelper;
+  }
+
+  public WebDriver getDriver() {
+    if (wd == null) {
       if (browser.equals(BrowserType.FIREFOX)) {
         wd = new FirefoxDriver();
       } else if (browser.equals(BrowserType.CHROME)) {
@@ -36,17 +61,9 @@ public class ApplicationManager  {
       } else if (browser.equals(BrowserType.IE)){
         wd = new InternetExplorerDriver();
       }
-      wd.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+      wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
       wd.get(properties.getProperty("web.baseUrl"));
-
-
     }
-
-    public void logout() {
-      wd.findElement(By.linkText("Logout")).click();
-    }
-    public void stop() {
-      wd.quit();
-    }
-
+    return wd;
   }
+}

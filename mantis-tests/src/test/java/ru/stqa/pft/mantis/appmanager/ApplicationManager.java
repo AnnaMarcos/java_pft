6aef47e2cebcb9b8fe572afdf548a4e86e20d82a
AnnaMarcos <58533836+AnnaMarcos@users.noreply.github.com>
2020-09-25 20:32:29 -0700

    8.5. Transferring files to a remote machine via FTP(RegistrationTest)

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -20,6 +20,8 @@ public class ApplicationManager {
 
   private  String browser;
   private RegistrationHelper registrationHelper;
+  private FtpHelper ftp;
+
 
   public ApplicationManager(String browser) {
     this.browser = browser;
@@ -68,9 +70,14 @@ public class ApplicationManager {
       registrationHelper = new RegistrationHelper(this);
     }
     return registrationHelper;
-
   }
 
+  public FtpHelper ftp() {
+    if (ftp == null) {
+      ftp = new FtpHelper(this);
+    }
+    return ftp;
+  }
 
   public WebDriver getDriver() {
     if (wd == null) {

145b9c03888e79c4843301b1f613ecefa5b37a1b
AnnaMarcos <58533836+AnnaMarcos@users.noreply.github.com>
2020-10-07 21:25:02 -0700

    8.7.Creating users  and Receiving mail to an external mail server. (via Telnet protocol)

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -21,6 +21,8 @@ public class ApplicationManager {
   private RegistrationHelper registrationHelper;
   private FtpHelper ftp;
   private MailHelper mailHelper;
+  private JamesHelper jamesHelper;
+
 
 
   public ApplicationManager(String browser) {
@@ -98,5 +100,11 @@ public class ApplicationManager {
       mailHelper = new MailHelper(this);
     }
     return  mailHelper;
+  }
+  public JamesHelper james() {
+    if (jamesHelper == null){
+      jamesHelper = new JamesHelper(this);
+    }
+    return jamesHelper;
   }
 }
